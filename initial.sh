38ff289bee123bc9d5ded9968a73eefd8ff635fb
Avelino <thiago@avelino.xxx>
2016-09-27 22:42:09 -0300

    run docker-compose after install

--- initial.sh
+++ initial.sh
@@ -21,3 +21,4 @@ service docker start
 docker run hello-world
 curl -L https://github.com/docker/compose/releases/download/1.8.0/run.sh > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+docker-compose
