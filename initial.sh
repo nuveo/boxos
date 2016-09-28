c4bc7fe0f331a4b53e0fff38476b760f65bd441c
Avelino <thiago@avelino.xxx>
2016-09-27 22:39:57 -0300

    remove apt_url, focus on jessie

--- initial.sh
+++ initial.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-apt_url="https://apt.dockerproject.org"
 gpg_fingerprint="58118E89F3A912897C070ADBF76221572C52609D"
 key_servers="
 ha.pool.sks-keyservers.net
 pgp.mit.edu
 keyserver.ubuntu.com
 "
-lsb_dist=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'id' | cut -d ':' -f 2 | tr -d '[[:space:]]')
-dist_version=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'codename' | cut -d ':' -f 2 | tr -d '[[:space:]]')
 
 aptitude update && aptitude upgrade
 aptitude install sudo apt-transport-https ca-certificates
@@ -18,7 +15,7 @@ for key_server in $key_servers ; do
 done
 apt-key adv -k ${gpg_fingerprint} >/dev/null
 mkdir -p /etc/apt/sources.list.d
-echo deb \[arch=$(dpkg --print-architecture)\] ${apt_url}/repo ${lsb_dist}-${dist_version} ${repo} > /etc/apt/sources.list.d/docker.list
+echo deb https://apt.dockerproject.org/repo debian-jessie main > /etc/apt/sources.list.d/docker.list
 sleep 3; aptitude update; aptitude install -y -q docker-engine
 service docker start
 docker run hello-world
