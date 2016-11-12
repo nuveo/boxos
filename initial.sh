39983ac0893e10615a48938aec6a887e7496e00c
Avelino <thiago@avelino.xxx>
2016-11-11 23:35:51 -0200

    fixed PS1 bash color

--- initial.sh
+++ initial.sh
@@ -71,6 +71,5 @@ chmod +x /usr/local/bin/docker-compose
 docker-compose
 
 echo "CUSTOM BASH"
-echo 'PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$"' > /root/.bash_profile
-echo 'alias ll="ls --color -l"' >> /root/.bash_profile
-set -o history
+echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\w \$ \[\033[00m\]'" > /root/.bash_profile
+echo "alias ll='ls --color -l'" >> /root/.bash_profile
